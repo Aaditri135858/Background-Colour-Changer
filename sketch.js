c4e8bf5ab11a4fdb4f3b3fb484f6c0483543aeea
Aaditri135858 <85681782+Aaditri135858@users.noreply.github.com>
2021-07-03 12:34:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+function draw(){
+  if(keyIsDown(DOWN_ARROW)){
+    background("red")
+  }
+  if(keyIsDown(UP_ARROW)){
+    background("green")
+  }
+}
